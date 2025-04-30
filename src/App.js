1f5e8d0c401dfd923447c063c223e863656bab2c
Gayathri Krishnan <iamkgayu@gmail.com>
2025-04-30 23:42:11 +0100

    Upgrading React Router to V6+

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NewPost from './NewPost';
 import PostPage from './PostPage';
 import About from './About';
 import Missing from './Missing';
-import {Route, Routes, useHistory} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route , useHistory } from 'react-router-dom';
 import {useState, useEffect} from 'react';
 
 
@@ -15,19 +15,15 @@ function App() {
     <div className="App">
       <Header title="React JS Blog"/>
       <Nav />
+      <Router>
       <Routes>
-        <Route exact path="/">
-         <Home />
-        </Route>
-        <Route exact path="/post">
-          <NewPost />
-        </Route>
-        <Route path="/post/:id">
-         <PostPage />
-        </Route>
-        <Route path="/about" component={About} />
-        <Route path="*" component={Missing} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/post" element={<NewPost/>} />
+        <Route path="/post/:id" element={<PostPage/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="*" element={<Missing/>} />
       </Routes>
+      </Router>
       <Footer />
     </div>
   );
